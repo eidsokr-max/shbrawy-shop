4c7a90c75eb812163661142e16fb1242bebd5168
eidsokr-max <eidsokr@gmail.com>
2026-07-08 14:21:58 +0300

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-import {db} from './firebase.js';
-document.getElementById('loginBtn').onclick=()=>document.getElementById('adminPanel').classList.remove('hidden');
-document.getElementById('saveBtn').onclick=()=>alert('ضع كود Firebase الحالي هنا لإضافة/تعديل المنتجات');
